0179f5dc84ee0c823ad2943e5118aaadeac63156
Harsh-wadile <harshal.wadile@anuyat.com>
2024-01-11 09:53:49 +0530

    first commit

---
+++ src/app/active-plan/active-plan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-active-plan',
+  standalone: true,
+  imports: [],
+  templateUrl: './active-plan.component.html',
+  styleUrl: './active-plan.component.css'
+})
+export class ActivePlanComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ActivePlanComponent } from './active-plan/active-plan.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,ActivePlanComponent,NavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
